4d880aedfa32030fb6dd4334c9b9bc3f08bee10d
codeallthethingz <howdoicontactwill@gmail.com>
2019-06-05 10:32:17 -0700

    ngrok rsync tmux

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 
 brewCask=("google-chrome" "1password" "iterm2" "slack" "dbeaver-community" "expressvpn" "visual-studio-code" "intellij-idea-ce" "steam" "vlc" "qbittorrent" "zoomus" "java" "goland" "ngrok")
-brew=("git" "bat" "zsh" "z" "vim" "docker" "docker-compose" "docker-machine" "xhyve" "docker-machine-driver-xhyve" "wget" "curl" "htop" "pipenv" "gcc" "tree" "jq" "postgres" "coreutils" "r" "rsync")
+brew=("git" "bat" "zsh" "z" "vim" "docker" "docker-compose" "docker-machine" "xhyve" "docker-machine-driver-xhyve" "wget" "curl" "htop" "pipenv" "gcc" "tree" "jq" "postgres" "coreutils" "r" "rsync" "tmux")
 npmGlobals=("now" "marko-cli" "http-server" "lasso-cli")
 
 # Annoying macos stuff
