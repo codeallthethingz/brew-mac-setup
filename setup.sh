7cb74b565d4e9fad2aafc2b687641a0879c2d54b
codeallthethingz <howdoicontactwill@gmail.com>
2021-03-31 14:24:32 -0700

    added golang

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 
 brewCask=("brave-browser" "1password" "iterm2" "slack" "expressvpn" "visual-studio-code" "steam" "vlc" "qbittorrent" "zoomus" "goland" "ngrok" "adoptopenjdk/openjdk/adoptopenjdk8" "docker")
-brew=("git" "bat" "zsh" "z" "vim" "wget" "curl" "htop" "pipenv" "gcc" "tree" "jq" "postgres" "coreutils" "r" "rsync" "tmux" "maven" "watch" "gdrive" "go-task/tap/go-task" "goreleaser" "pandoc" "rename" "hub" "sqlite")
+brew=("go" "git" "bat" "zsh" "z" "vim" "wget" "curl" "htop" "pipenv" "gcc" "tree" "jq" "postgres" "coreutils" "r" "rsync" "tmux" "maven" "watch" "gdrive" "go-task/tap/go-task" "goreleaser" "pandoc" "rename" "hub" "sqlite")
 npmGlobals=("vercel" "http-server" "npm-check-updates")
 
 # Annoying macos stuff
